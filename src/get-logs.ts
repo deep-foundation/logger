3d2bdca8b74d95e46cf37a21f2028b8e71a33ec9
FreePhoenix888 <freephoenix888@gmail.com>
2023-08-16 17:10:52 +0600

    Update getLogs

--- src/get-logs.ts
+++ src/get-logs.ts
@@ -1,17 +1,24 @@
 import { DeepClient } from "@deep-foundation/deeplinks/imports/client";
+import { BoolExpLink } from "@deep-foundation/deeplinks/imports/client_types";
 import { Package } from "./package";
 
 export async function getLogs(options: GetLogsOptions) {
   const { linkId, deep } = options;
   const _package = new Package({ deep });
   const typeLinkIds = await getTypeLinkIds({_package,typeLinkIds: options.typeLinkIds});
-  const { logTreeTypeLinkId } = typeLinkIds;
-  return await deep.select({
+  const { logTreeTypeLinkId,logIdTypeLinkId } = typeLinkIds;
+  const selectData: BoolExpLink = {
     down: {
       tree_id: logTreeTypeLinkId,
-      link_id: linkId
-    }
-  })
+      parent: {
+        out: {
+          type_id: logIdTypeLinkId,
+          to_id: linkId,
+        },
+      },
+    },
+  };
+  return await deep.select(selectData)
 }
 
 export interface GetLogsOptions {
@@ -19,6 +26,7 @@ export interface GetLogsOptions {
   linkId: number;
   typeLinkIds?: {
     logTreeTypeLinkId?: number;
+    logIdTypeLinkId?: number;
   };
 }
 
@@ -27,10 +35,11 @@ interface GetTypeLinkIdsOptions {
   _package: Package;
 }
 
-type GetTypeLinkIdsResult = Exclude<GetLogsOptions['typeLinkIds'], undefined>;
+type GetTypeLinkIdsResult = Required<Exclude<GetLogsOptions['typeLinkIds'], undefined>>;
 
 async function getTypeLinkIds(options: GetTypeLinkIdsOptions): Promise<GetTypeLinkIdsResult> {
   return {
-    logTreeTypeLinkId: options.typeLinkIds?.logTreeTypeLinkId ?? await options._package.LogTree.id()
+    logTreeTypeLinkId: options.typeLinkIds?.logTreeTypeLinkId ?? await options._package.LogTree.id(),
+    logIdTypeLinkId: options.typeLinkIds?.logIdTypeLinkId ?? await options._package.LogId.id(),
   }
 }
